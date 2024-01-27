44cae3ee8e4bdf3c3fd615289ea29a32226e5e79
Riki Wendri <fayyadh.farizzi@gmail.com>
2024-01-27 14:57:21 +0700

    add access control allow origin

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// const jsonServer = require("json-server");
-// const server = jsonServer.create();
-// const router = jsonServer.router("db.json");
-// const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 3001;
-
-// server.use(middlewares);
-// server.use(router);
-// server.listen(port, () => {
-//   console.log(`JSON Server is running on port ${port}`);
-// })
-
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
@@ -17,9 +5,15 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001;
 
 // Enable CORS for all routes
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
   next();
 });
 
